209861cd81bd10b5ec19ce5b4aef68adfe08e85d
Panajotis Zamos <aqw137@gmail.com>
2018-12-15 01:24:26 +0100

    added composer

---
+++ Bar.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: panos
+ * Date: 12/15/18
+ * Time: 12:09 AM
+ */
+
+namespace panos-zamos\foo;
+
+
+class Bar
+{
+
+    static function eureka()
+    {
+        die('Eureka');
+    }
+}
